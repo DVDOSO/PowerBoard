5f4a8def1cc8dfe35bb38bddc3255ca5cd75afcc
DVDOSO <davidjiang0307@gmail.com>
2023-09-21 15:27:00 -0400

    Add basic dashboard UI

---
+++ src/components/NavbarPrivate.svelte
@@ -0,0 +1,53 @@
+<script>
+    import { goto } from '$app/navigation';
+    import { authHandlers, authStore } from '../stores/authStore.js';
+    async function routeHome() {
+        goto('/')
+    }
+
+</script>
+
+<div class='containerNav'>
+    <ul>
+        <li><button on:click={routeHome}>Home</button></li>
+        <div class='auth'>
+            <li><button>Settings</button></li>
+            <li><button on:click={authHandlers.logout}>Log Out</button></li>
+        </div>
+    </ul>
+</div>
+
+<style>
+    .containerNav {
+        padding: 10px;
+        background-color:rgb(144, 233, 255);
+        position: fixed;
+        top: 0;
+        width: 100%;
+    }
+    ul, li {
+        display: inline;
+    }
+    button {
+        border-color: rgb(154, 181, 255);
+        background-color: transparent;
+        border: black solid 1px;
+        color: black;
+        padding: 10px;
+        margin-right: 5px;
+        margin-left: 5px;
+        list-style-type: none;
+        font-size: 20px;
+        cursor: pointer;
+        border-radius: 5px;
+        transition-duration: 0.4s;
+    }
+    button:hover {
+        background-color: rgb(154, 181, 255);
+        color: white;
+    }
+    .auth {
+        float: right;
+        display: inline;
+    }
+</style>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,6 @@
 <script>
-    import AuthReset from "../../components/AuthReset.svelte";
-    import  { auth } from "../../lib/firebase/firebase.client";
-	import { authStore, authHandlers } from "../../stores/authStore";
+	import { authHandlers, authStore } from '../../stores/authStore.js';
+    import NavbarPrivate from "../../components/NavbarPrivate.svelte";
 
     let email;
     authStore.subscribe((curr) => {
@@ -10,9 +9,13 @@
 </script>
 
 {#if $authStore.currentUser !== null}
-    <div>
-        <h1>Welcome {email}!</h1>
-        <AuthReset/>
+    <NavbarPrivate/>
+    <div class='webpage'>
+        <div class='containerMain'>
+            <h1 class='title'>
+                Welcome, {email}!
+            </h1>
+        </div>
     </div>
 {:else}
     <div>
@@ -21,14 +24,20 @@
 {/if}
 
 <style>
-    /*div {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }*/
-    h1 {
+    .webpage {
+        height: 100vh;
+        overflow: hidden;
+    }
+    .containerMain {
         text-align: center;
+        margin: 0;
+        padding: 10px;
+        background-color: rgb(210, 246, 255);
+        width: 100%;
+        height: 300vh;
+    }
+    .title {
+        font-size: 60px;
+        margin-top: 35vh;
     }
 </style>
