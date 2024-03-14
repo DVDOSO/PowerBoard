f736ff1ba806adff7692af261ec85060b2ccc4bf
DVDOSO <davidjiang0307@gmail.com>
2024-03-13 20:28:08 -0400

    Private UI

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -25,9 +25,17 @@
     }
 </script>
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
 <NavbarGuest/>
 
 <div class='webpage'>
+    <div class='box'></div>
     <div class='containerMain'>
         <h1 class='title'>
             Log In
@@ -51,12 +59,14 @@
     .webpage {
         height: 100vh;
         overflow: hidden;
+        background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+        background-size: 400% 400%;
+        animation: gradient 15s ease infinite;
     }
     .containerMain {
         text-align: center;
         margin: 0;
         padding: 10px;
-        background-color: rgb(210, 246, 255);
         width: 100%;
         height: 300vh;
     }
@@ -64,6 +74,18 @@
         font-size: 40px;
         margin-top: 30vh;
         margin-bottom: 3vh;
+        font-family: "Mulish", sans-serif;
+        color: white;
+    }
+    .box{
+        border: 2px dashed white;
+        width: 30vw;
+        height: 50vh;
+        left: 35vw;
+        top: 23vh;
+        position: fixed;
+        border-radius: 2vh;
+        pointer-events: none;
     }
     label {
         display: block;
@@ -73,14 +95,20 @@
     input {
         padding: 1vh;
         width: 15vw;
-        
+        border-radius: 5px;
+        border: white solid 1px;
+        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+    input:focus {
+        outline-color: rgb(0, 22, 135);
+        outline-style: double;
+        outline-width: 1px;
     }
     .submitButton {
         margin-top: 1vh;
-        border-color: rgb(154, 181, 255);
         background-color: transparent;
-        border: black solid 1px;
-        color: black;
+        border: rgb(255, 255, 255) solid 1px;
+        color: rgb(255, 255, 255);
         padding: 10px;
         margin-right: 5px;
         margin-left: 5px;
@@ -89,21 +117,38 @@
         cursor: pointer;
         border-radius: 5px;
         transition-duration: 0.4s;
+        font-family: 'Mulish', sans-serif;
     }
     .submitButton:hover {
-        background-color: rgb(154, 181, 255);
-        color: white;
+        background-color: rgb(255, 255, 255);
+        color: rgb(85, 85, 85);
+        box-shadow: 0 0 3vh rgba(255, 255, 255, .5);
     }
     p {
         margin-top: 2vh;
         display: inline;
+        color: white;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     }
     a {
         margin-top: 2vh;
         display: inline-block;
+        text-decoration: none;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     }
     .forgotPassword {
         display:block;
         margin-top: 2vh;
+    }
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
     }
 </style>
