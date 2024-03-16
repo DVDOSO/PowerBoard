d3e7b6979e2fe5d8169bebb15338f306b04d2bcb
DVDOSO <davidjiang0307@gmail.com>
2024-03-16 14:01:28 -0400

    Edit html titles
    Now fully functional!

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -31,6 +31,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+<title>PowerBoard</title>
 
 <NavbarGuest/>
 
