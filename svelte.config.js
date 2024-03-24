4a3ca913150a2c3a5228b82a8b24477dfd80535b
DVDOSO <davidjiang0307@gmail.com>
2024-03-24 15:45:35 -0400

    Revert "Deploy test"
    This reverts commit a08d20f3db41ba4d212afde19be8a22414154873.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
-
-const dev = process.argv.includes('dev');
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +6,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
-		}
+		adapter: adapter()
 	}
 };
 
