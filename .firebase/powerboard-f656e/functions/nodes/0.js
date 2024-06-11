c83f16b9bfcf3fad11ad3fadc338b8a448d18fdb
David Jiang <35905459+DVDOSO@users.noreply.github.com>
2024-06-11 18:42:31 -0400

    Delete .firebase directory

--- .firebase/powerboard-f656e/functions/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.9e769202.js","_app/immutable/chunks/scheduler.f64133fb.js","_app/immutable/chunks/index.dc929e09.js","_app/immutable/chunks/authStore.cad14b08.js","_app/immutable/chunks/index.a4d72958.js"];
-export const stylesheets = ["_app/immutable/assets/0.9bdb8cad.css"];
-export const fonts = [];
